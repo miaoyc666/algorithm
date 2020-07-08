d067fb458d89ccc31f25482a6e0300ab5b5cb109
miaoyc <miaoyc666@outlook.com>
2020-07-09 01:01:42 +0800

    ====update doc====

--- leetcode/array/_170_two-sum-iii-data-structure-design.py
+++ leetcode/array/_170_two-sum-iii-data-structure-design.py
@@ -5,7 +5,25 @@
 File name    : _170_two-sum-iii-data-structure-design.py
 Author       : miaoyc
 Create date  : 2020/6/21
-Description  : 
+Description  :
+"""
+
+"""
+设计并实现一个 TwoSum 的类，使该类需要支持 add 和 find 的操作。
+
+add 操作 -  对内部数据结构增加一个数。
+find 操作 - 寻找内部数据结构中是否存在一对整数，使得两数之和与给定的数相等。
+
+示例 1:
+
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+示例 2:
+
+add(3); add(1); add(2);
+find(3) -> true
+find(6) -> false
 """
 
 import collections
@@ -43,7 +61,6 @@ class TwoSum(object):
                 if value - i == i and self.data[i] >= 2:
                     return True
         return False
-
 
 # Your TwoSum object will be instantiated and called as such:
 # obj = TwoSum()
