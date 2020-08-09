bed984d8ec1f7b5144e720b668286bb61e1c620f
miaoyc <miaoyc666@outlook.com>
2020-08-09 16:41:17 +0800

    ====add string package; update doc====

---
+++ leetcode/string/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : __init__.py
+Author       : miaoyc
+Create date  : 2020/8/9 4:29 下午
+Description  : string algorithm
+"""
+
+
+
+
+
