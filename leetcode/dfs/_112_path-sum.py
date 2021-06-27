57d0310fa416c4e67736fe10b4c904c057d4e865
miaoyc <miaoyc666@outlook.com>
2021-06-28 00:38:50 +0800

    ====add dfs: path-sum====

--- leetcode/dfs/_112_path-sum.py
+++ leetcode/dfs/_112_path-sum.py
@@ -33,3 +33,42 @@ Description  : 路径总和
 -1000 <= targetSum <= 1000
 """
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution(object):
+#     def hasPathSum(self, root, targetSum):
+#         """
+#         :type root: TreeNode
+#         :type targetSum: int
+#         :rtype: bool
+#         """
+#         if not root:
+#             return False
+#         if not root.left and not root.right:
+#             return targetSum == root.val
+#         return self.a(root, targetSum - root.val)
+
+#     def a(self, root, targetSum):
+#         if not root:
+#             return False
+#         if not root.left and not root.right:
+#             return targetSum == root.val
+#         return self.a(root.left, targetSum - root.val) or self.a(root.right, targetSum - root.val)
+
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return sum == root.val
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
