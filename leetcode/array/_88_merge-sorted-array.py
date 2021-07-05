1b31a262f8e003bfe13cb55991e4943a500c2850
miaoyc <miaoyc666@outlook.com>
2021-07-06 00:32:02 +0800

    ====add array: merge-sorted-array====

--- leetcode/array/_88_merge-sorted-array.py
+++ leetcode/array/_88_merge-sorted-array.py
@@ -7,3 +7,58 @@ Author       : miaoyc
 Create date  : 2021/7/5 11:54 下午
 Description  : 合并两个有序数组
 """
+
+"""
+给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+输出：[1,2,2,3,5,6]
+示例 2：
+
+输入：nums1 = [1], m = 1, nums2 = [], n = 0
+输出：[1]
+ 
+
+提示：
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[i] <= 109
+"""
+
+
+class Solution(object):
+
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        # 思路：双指针从后向前遍历，原地替换，无需新空间
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] >= nums2[j]:
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
+        while j >= 0:
+            nums1[k]= nums2[j]
+            k -= 1
+            j -= 1
