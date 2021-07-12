9ce531e04164ff791b10d1c5f192c2f585131bfd
miaoyc <miaoyc666@outlook.com>
2021-07-13 00:45:11 +0800

    ====add dfs: merge-two-binary-trees====

--- leetcode/dfs/_617_merge-two-binary-trees.py
+++ leetcode/dfs/_617_merge-two-binary-trees.py
@@ -33,3 +33,53 @@ Description  : 合并二叉树
 
 """
 
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution(object):
+
+    def mergeTrees(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: TreeNode
+        """
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+        return root1
+
+    #     # if not root1 and not root2:
+    #     #     return
+    #     # root = TreeNode(root1.val + root2.val)
+    #     self.root = self.dfs(self.root, root1, root2)
+    #     return self.root
+
+    # def dfs(self, root, root1, root2):
+    #     if not root1 and not root2:
+    #         return
+
+    #     if root1 and root2:
+    #         if not root:
+    #             root = TreeNode(0)
+    #         root.val = root1.val + root2.val
+    #         root.left =self.dfs(root.left, root1.left, root2.left)
+    #         root.right = self.dfs(root.right, root1.right, root2.right)
+    #         return root
+
+    #     if root1 and not root2:
+    #         return TreeNode(root1.val)
+
+    #     if not root1 and root2:
+    #         return TreeNode(root2.val)
