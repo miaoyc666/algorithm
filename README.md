3bd4d712166487bd612beb0fcfda8e0c0cc10f56
miaoyc <miaoyc666@outlook.com>
2021-07-19 23:07:20 +0800

    ====add array: binary-search====

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 刷题记录
 
 【当前进度】  
-leetcode：43  
+leetcode：44  
 hihocoder：4  
   
 【说明】  
---
+++ leetcode/array/_704_binary-search.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _704_binary-search.py
+Author       : miaoyc
+Create date  : 2021/7/19 10:21 下午
+Description  : 二分查找
+"""
+
+"""
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+
+示例 1:
+
+输入: nums = [-1,0,3,5,9,12], target = 9
+输出: 4
+解释: 9 出现在 nums 中并且下标为 4
+示例 2:
+
+输入: nums = [-1,0,3,5,9,12], target = 2
+输出: -1
+解释: 2 不存在 nums 中因此返回 -1
+ 
+
+提示：
+
+你可以假设 nums 中的所有元素是不重复的。
+n 将在 [1, 10000]之间。
+nums 的每个元素都将在 [-9999, 9999]之间。
+"""
+
+
+class Solution(object):
+
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) / 2
+            if nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                return mid
+        return -1
