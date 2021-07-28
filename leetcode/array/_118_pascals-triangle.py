febe91c5bae7919b0b197647da1327fb899d97a9
miaoyc <miaoyc666@outlook.com>
2021-07-28 22:50:30 +0800

    ====init array: pascals-triangle====

---
+++ leetcode/array/_118_pascals-triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _118_pascals-triangle.py
+Author       : miaoyc
+Create date  : 2021/7/28 10:49 下午
+Description  : 杨辉三角
+"""
+
+"""
+给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+
+在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+
+示例 1:
+
+输入: numRows = 5
+输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+示例 2:
+
+输入: numRows = 1
+输出: [[1]]
+ 
+
+提示:
+
+1 <= numRows <= 30
+
+"""
+
