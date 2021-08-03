40f4898b33bcdeb5213d884213feb89146f61921
miaoyc <miaoyc666@outlook.com>
2021-08-03 23:51:38 +0800

    ====add linked-list: merge-two-sorted-lists====

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 刷题记录
 
 【当前进度】  
-leetcode：66                                    
+leetcode：67                                      
 hihocoder：4  
   
 【说明】  
---
+++ leetcode/linked-list/_21_merge-two-sorted-lists.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _21_merge-two-sorted-lists.py
+Author       : miaoyc
+Create date  : 2021/8/3 11:49 下午
+Description  : 合并两个有序链表
+"""
+
+"""
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+示例 1：
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+
+提示：
+
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode()
+        pre = head
+        while l1 and l2:
+            if l1.val <= l2.val:
+                pre.next = l1
+                l1 = l1.next
+            else:
+                pre.next = l2
+                l2 = l2.next
+            pre = pre.next
+        pre.next = l1 if l1 else l2
+        return head.next
