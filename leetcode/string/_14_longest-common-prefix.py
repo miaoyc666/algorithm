6c9cdcb89823555d2d027c0154c7fe549aa9cf1a
miaoyc <miaoyc666@outlook.com>
2021-08-04 23:56:13 +0800

    ====init string: longest-common-prefix====

---
+++ leetcode/string/_14_longest-common-prefix.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _14_longest-common-prefix.py
+Author       : miaoyc
+Create date  : 2021/8/4 11:55 下午
+Description  : 最长公共前缀
+"""
+
+"""
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串""。
+
+示例 1：
+
+输入：strs = ["flower","flow","flight"]
+输出："fl"
+示例 2：
+
+输入：strs = ["dog","racecar","car"]
+输出：""
+解释：输入不存在公共前缀。
+
+提示：
+
+0 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] 仅由小写英文字母组成
+"""
+
