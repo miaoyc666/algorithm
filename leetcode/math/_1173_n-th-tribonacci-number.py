be79f3847487d50c3c1408112baac1406ec50185
miaoyc <miaoyc666@outlook.com>
2021-08-07 22:48:02 +0800

    ====init math: n-th-tribonacci-numbe====

--- leetcode/math/_1173_n-th-tribonacci-number.py
+++ leetcode/math/_1173_n-th-tribonacci-number.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """
-File name    : _1173_n-th-tribonacci-numbe.py
+File name    : _1173_n-th-tribonacci-number.py
 Author       : miaoyc
-Create date  : 2021/8/8 10:48 下午
+Create date  : 2021/8/7 10:40 下午
 Description  : 第 N 个泰波那契数
 """
 
