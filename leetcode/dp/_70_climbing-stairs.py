efb9254f52ba328010b37436b1f62954ef1ef721
miaoyc <miaoyc666@outlook.com>
2021-08-27 23:37:26 +0800

    ====add dp: climbing-stairs====

--- leetcode/dp/_70_climbing-stairs.py
+++ leetcode/dp/_70_climbing-stairs.py
@@ -29,3 +29,15 @@ Description  : 爬楼梯
 2.  1 阶 + 2 阶
 3.  2 阶 + 1 阶
 """
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # dp[i]表示爬到第i级楼梯的种数， (1, 2) (2, 1)是两种不同的类型
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        for i in range(n+1):
+            for j in range(1, 3):
+                if i>=j:
+                    dp[i] += dp[i-j]
+        return dp[-1]
