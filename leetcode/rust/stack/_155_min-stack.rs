1a586653dc6bd39baf816fb4a986d09648c28d31
miaoyc <miaoyc666@outlook.com>
2021-09-08 00:06:09 +0800

    ====init stack: min-stack====

---
+++ leetcode/rust/stack/_155_min-stack.rs
@@ -0,0 +1,36 @@
+/*
+File name    : _155_min-stack.rs
+Author       : miaoyc
+Create date  : 2021/9/8 12:05 上午
+Description  : 最小栈
+*/
+
+/*
+
+"""
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+push(x) —— 将元素 x 推入栈中。
+pop()—— 删除栈顶的元素。
+top()—— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+
+示例:
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+"""
+*/
+
