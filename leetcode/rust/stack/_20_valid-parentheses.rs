19bfbfcf0190b646338295204b03637bdd937b51
miaoyc <miaoyc666@outlook.com>
2021-09-26 14:31:42 +0800

    ====add valid-parentheses comment====

--- leetcode/rust/stack/_20_valid-parentheses.rs
+++ leetcode/rust/stack/_20_valid-parentheses.rs
@@ -36,3 +36,20 @@ Description  :
 s 仅由括号 '()[]{}' 组成
 */
 
+/*
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        dic = {'{': '}',  '[': ']', '(': ')', '?': '?'}
+        stack = ['?']
+        for c in s:
+            if c in dic:
+                stack.append(c)
+            elif dic[stack.pop()] != c:
+                return False
+        return len(stack) == 1
+*/
+
