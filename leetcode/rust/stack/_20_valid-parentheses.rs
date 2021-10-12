706471be4d8c3ab29fdacce0458e4a4b2676dc1b
miaoyc <miaoyc666@outlook.com>
2021-10-12 23:47:55 +0800

    ====update <rust> stack: valid-parentheses====

--- leetcode/rust/stack/_20_valid-parentheses.rs
+++ leetcode/rust/stack/_20_valid-parentheses.rs
@@ -37,6 +37,7 @@ s 仅由括号 '()[]{}' 组成
 */
 
 /*
+以下为python实现，本打算用rust来按照此思路实现代码，但是对字典操作不是很熟悉
 class Solution(object):
     def isValid(self, s):
         """
@@ -51,5 +52,56 @@ class Solution(object):
             elif dic[stack.pop()] != c:
                 return False
         return len(stack) == 1
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut dic = HashMap::new();
+        let mut stack =  Vec::new();
+        stack.push('?');
+        for c in 0..s.chars() {
+            if dic.contains_key(&c) {
+
+            } else {
+                x = stack.pop()
+
+            }
+            if c in dic{}
+
+            match dic.get(&c) {
+                stack.push(c),
+
+                _ => println!("Don't have Daniel's number."),
+            }
+
+
+        }
+    }
+}
 */
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let chars: Vec<char> = s.chars().collect();
+        // 判断是否为空字符串，空字符串视为有效字符串
+        if chars.len() == 0 {
+            return true;
+        }
+        let mut stack: Vec<char> = Vec::new();
+        for i in 0..chars.len() {
+            if chars[i] == '(' {        // 如果字符是左小括号，将右小括号入栈
+                stack.push(')');
+            } else if chars[i] == '[' { // 如果字符是左中括号，将右中括号入栈
+                stack.push(']');
+            } else if chars[i] == '{' { // 如果字符是左大括号，将右大括号入栈
+                stack.push('}');
+            } else if stack.is_empty() || chars[i] != stack.pop().unwrap() {    // 栈为空或字符与栈顶元素不相同时，为无效字符
+                return false;
+            }
+        }
+        // 匹配结束，栈为空代表是有效字符串，否则是无效字符串
+        return stack.is_empty();
+    }
+}
 
