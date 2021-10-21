efa84c31abfb8bfbac1557fcde5be54cedea0cee
miaoyc <miaoyc666@outlook.com>
2021-10-21 23:53:07 +0800

    ====init linked-list: remove-linked-list-elements====

---
+++ leetcode/python/linked-list/_203_remove-linked-list-elements.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _203_remove-linked-list-elements.py
+Author       : miaoyc
+Create date  : 2021/10/21 11:51 下午
+Description  : 
+"""
+
+"""
+给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+
+示例 1：
+输入：head = [1,2,6,3,4,5,6], val = 6
+输出：[1,2,3,4,5]
+
+示例 2：
+输入：head = [], val = 1
+输出：[]
+
+示例 3：
+输入：head = [7,7,7,7], val = 7
+输出：[]
+"""
