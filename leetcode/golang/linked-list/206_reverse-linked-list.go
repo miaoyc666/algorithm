951fd49ef877906d8724f9169df3a420614864a8
miaoyc <miaoyc666@outlook.com>
2021-11-05 13:09:26 +0800

    ====add <golang>: linked-list: reverse-linked-list====

---
+++ leetcode/golang/linked-list/206_reverse-linked-list.go
@@ -0,0 +1,55 @@
+package linked_list
+
+/*
+File name    : 206_reverse-linked-list.go
+Author       : miaoyc
+Create date  : 2021/11/5 1:02 下午
+Description  : 反转链表
+*/
+
+/*
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+示例 1：
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+
+示例 2：
+输入：head = [1,2]
+输出：[2,1]
+
+示例 3：
+输入：head = []
+输出：[]
+
+提示：
+链表中节点的数目范围是 [0, 5000]
+-5000 <= Node.val <= 5000
+
+进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ */
+
+/**
+* Definition for singly-linked list.
+* type ListNode struct {
+*     Val int
+*     Next *ListNode
+* }
+*/
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
+}
