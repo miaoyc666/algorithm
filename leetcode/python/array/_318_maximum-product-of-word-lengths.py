b48cc0d7bd6f3e1592abd17f5d9889cb9d272029
miaoyc <miaoyc666@outlook.com>
2021-11-17 23:57:34 +0800

    ====init array: maximum-product-of-word-lengths====

---
+++ leetcode/python/array/_318_maximum-product-of-word-lengths.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _318_maximum-product-of-word-lengths.py
+Author       : miaoyc
+Create date  : 2021/11/17 11:57 下午
+Description  : 最大单词长度乘积
+"""
