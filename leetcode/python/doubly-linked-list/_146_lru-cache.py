314702ad901aa45670df66404409d44ddc79b66f
miaoyc <miaoyc666@outlook.com>
2021-12-10 22:44:49 +0800

    ====refactor====

--- leetcode/python/doubly-linked-list/_146_lru-cache.py
+++ leetcode/python/doubly-linked-list/_146_lru-cache.py
@@ -11,16 +11,13 @@ Description  : LRU 缓存机制
 """
 运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制 。
 实现 LRUCache 类：
-
 LRUCache(int capacity) 以正整数作为容量capacity 初始化 LRU 缓存
 int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
 void put(int key, int value)如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
-
 进阶：你是否可以在O(1) 时间复杂度内完成这两种操作？
 
 
 示例：
-
 输入
 ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
 [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
@@ -40,7 +37,6 @@ lRUCache.get(3);    // 返回 3
 lRUCache.get(4);    // 返回 4
 
 提示：
-
 1 <= capacity <= 3000
 0 <= key <= 10000
 0 <= value <= 105
