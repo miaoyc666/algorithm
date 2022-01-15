eaeaa99484e5f4f3c486ecf4ca915a9e7a032d1a
miaoyc <miaoyc666@outlook.com>
2022-01-15 12:49:41 +0800

    ====refactor; init bfs: binary-tree-level-order-traversal====

---
+++ leetcode/python/bfs/_102_binary-tree-level-order-traversal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _102_binary-tree-level-order-traversal.py
+Author       : miaoyc
+Create date  : 2022/1/15 12:46 下午
+Description  : 二叉树层序遍历
+"""
+
+"""
+难度：中等
+
+从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+
+例如:
+给定二叉树:[3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回：
+[3,9,20,15,7]
+
+提示：
+节点总数 <= 1000
+"""
---
+++ leetcode/python/bfs/jz-offer2-32_cong-shang-dao-xia-da-yin-er-cha-shu.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _102_cong-shang-dao-xia-da-yin-er-cha-shu.py
+Author       : miaoyc
+Create date  : 2022/1/15 12:46 下午
+Description  : 从上到下打印二叉树
+"""
+
+"""
+难度：中等
+
+与102binary-tree-level-order-traversal 题目重复
+ 
+从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+
+例如:
+给定二叉树:[3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回：
+[3,9,20,15,7]
+
+提示：
+节点总数 <= 1000
+"""
