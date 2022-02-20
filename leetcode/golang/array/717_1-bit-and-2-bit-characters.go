45df285b22f307451e8dadfcc72a0f776aee5ec1
miaoyc <miaoyc666@outlook.com>
2022-02-21 00:11:17 +0800

    ====add array: 1-bit-and-2-bit-characters====

--- leetcode/golang/array/717_1-bit-and-2-bit-characters.go
+++ leetcode/golang/array/717_1-bit-and-2-bit-characters.go
@@ -31,3 +31,24 @@ Description  : 1比特与2比特字符
 1 <= bits.length <= 1000
 bits[i] == 0 or 1
 */
+
+/*
+解题思路：
+从头开始遍历，当字符是1时，步进为2，当字符是0时，步进为1，遍历退出条件为能否遍历到最后一个字符。
+*/
+
+func isOneBitCharacter(bits []int) bool {
+	l := len(bits)
+	i := 0
+	for i < l {
+		if i == l-1 {
+			return true
+		}
+		if bits[i] == 1 {
+			i += 2
+		} else {
+			i += 1
+		}
+	}
+	return false
+}
