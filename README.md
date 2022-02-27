06d6903f848839f435c633e72293a77f5125b6f7
miaoyc <miaoyc666@outlook.com>
2022-02-28 00:50:12 +0800

    ====add binary-search: find-first-and-last-position-of-element-in-sorted-array====

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 - 类型题解题模板单独按类型整理：[解题模板](./doc/template/template.md) 
 
 ###当前进度  
-- leetcode algorithms: 121（重复题目：7）
+- leetcode algorithms: 122（重复题目：7）
 - hihocoder: 4
 - leetcode database: 1
 
--- leetcode/python/binary-search/_34_find-first-and-last-position-of-element-in-sorted-array.py
+++ leetcode/python/binary-search/_34_find-first-and-last-position-of-element-in-sorted-array.py
@@ -35,3 +35,58 @@ nums是一个非递减数组
 -109<= target<= 109
 """
 
+"""
+解题思路：
+两次二分查找，一次找到头的索引，一次找到尾的索引
+由于存在重复元素，退出条件就不再是nums[mid] == target
+当查找左边界时，出现判等后，high=mid-1继续查找，最终返回low
+当查找右边界时，出现判等后，low=mid+1继续查找，最终返回high
+"""
+
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 0 or nums[0] > target or nums[-1] < target:
+            return [-1, -1]
+        return [self.find_left(nums, target), self.find_right(nums, target)]
+
+    def find_left(self, nums: List[int], target: int):
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            # 如果 nums[mid] = target，继续向左寻找左边界
+            if nums[mid] == target:
+                high = mid - 1
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        if nums[low] == target:
+            return low
+        else:
+            return -1
+
+    def find_right(self, nums: List[int], target: int):
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            # 如果 nums[mid] = target，继续向右寻找右边界
+            if nums[mid] == target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        if nums[high] == target:
+            return high
+        else:
+            return -1
+
+
+if __name__ == "__main__":
+    a = [5, 7, 7, 8, 8, 10]
+    b = 8
+    print(Solution().find_left(a, b))
+    print(Solution().find_right(a, b))
