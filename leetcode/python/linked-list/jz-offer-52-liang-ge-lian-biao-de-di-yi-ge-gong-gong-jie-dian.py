ea4f505a915aaf4a065736f76d00455173757373
miaoyc <miaoyc666@outlook.com>
2022-03-06 00:02:17 +0800

    ====update linked-list: liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian====

---
+++ leetcode/python/linked-list/jz-offer-52-liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : jz-offer-52-liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian.py
+Author       : miaoyc
+Create date  : 2022/3/5 11:58 PM
+Description  : 两个链表的第一个公共节点
+"""
+
+"""
+难度：简单
+与160 intersection-of-two-linked-lists题目重复
+
+输入两个链表，找出它们的第一个公共节点。
+如下面的两个链表：
+在节点 c1 开始相交。
+
+示例 1：
+输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+输出：Reference of the node with value = 8
+输入解释：相交节点的值为 8 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+
+示例2：
+输入：intersectVal= 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+输出：Reference of the node with value = 2
+输入解释：相交节点的值为 2 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+
+示例3：
+
+输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+输出：null
+输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+解释：这两个链表不相交，因此返回 null。
+
+注意：
+如果两个链表没有交点，返回 null.
+在返回结果后，两个链表仍须保持原有的结构。
+可假定整个链表结构中没有循环。
+程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+"""
+
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        a, b = headA, headB
+        while a and b:
+            if a == b:
+                return a
+            a = a.next
+            b = b.next
+            if not a and not b:
+                return None
+            if not a:
+                a = headB
+            if not b:
+                b = headA
