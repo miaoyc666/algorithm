f57429aaf6f0e966fbf454b51d447c6def429f48
miaoyc <miaoyc666@outlook.com>
2022-03-20 00:35:27 +0800

    ====update binary-tree: construct-string-from-binary-tree====

--- leetcode/python/binary-tree/_606_construct-string-from-binary-tree.py
+++ leetcode/python/binary-tree/_606_construct-string-from-binary-tree.py
@@ -4,7 +4,8 @@
 """
 File name    : _606_construct-string-from-binary-tree.py
 Author       : miaoyc
-Create date  : 2022/3/19 11:47 PM
+Create date  : 2021/7/12 1:55 上午
+Update date  : 2022/3/20 0:34 上午
 Description  : 根据二叉树创建字符串
 """
 
@@ -12,7 +13,6 @@ Description  : 根据二叉树创建字符串
 难度：简单
 
 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
-
 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
 
 示例 1:
@@ -43,3 +43,58 @@ Description  : 根据二叉树创建字符串
 解释: 和第一个示例相似，
 除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。
 """
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+# class Solution(object):
+#
+#     def tree2str(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: str
+#         """
+#         if not root:
+#             return ""
+#         if not root.left and not root.right:
+#             return "{0}".format(root.val)
+#         if not root.right:
+#             return "{0}".format(root.val) + "(" + self.tree2str(root.left) + ")"
+#         return "{0}".format(root.val) + "(" + self.tree2str(root.left) + ")(" + self.tree2str(root.right) + ")"
+#
+
+class Solution:
+
+    def __init__(self):
+        self.res = ""
+
+    def tree2str(self, root) -> str:
+        self.preorder(root)
+        return self.res[1:-1]
+
+    def preorder(self, root):
+        if not root:
+            return
+
+        self.res += "("
+
+        # val
+        self.res += str(root.val)
+
+        # left, 只有左子树需要填充
+        if not root.left and root.right:
+            self.res += "()"
+
+        if root.left:
+            self.preorder(root.left)
+
+        # right
+        if root.right:
+            self.preorder(root.right)
+
+        self.res += ")"
