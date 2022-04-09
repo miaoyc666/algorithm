48d5f5c63f1a49746cc99ee6d046cf1bde08e7b9
miaoyc <miaoyc666@outlook.com>
2022-04-09 23:48:04 +0800

    ====add hash-table: contains-duplicate-ii====

--- leetcode/python/hash-table/_219_contains-duplicate-ii.py
+++ leetcode/python/hash-table/_219_contains-duplicate-ii.py
@@ -31,4 +31,14 @@ Description  : 存在重复元素 II
 0 <= k <= 105
 """
 
+from typing import List
 
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        pos = {}
+        for i, num in enumerate(nums):
+            if num in pos and i - pos[num] <= k:
+                return True
+            pos[num] = i
+        return False
