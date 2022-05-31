5b3509f63c38e8da02aa08663f85588cc6cf50cd
miaoyc <miaoyc666@outlook.com>
2022-05-31 23:28:30 +0800

    ====add dfs: alien-dictionary====

---
+++ leetcode/python/dfs/jz-offer-114-Jf1JuT.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : jz-offer-114-Jf1JuT.py
+Author       : miaoyc
+Create date  : 2022/5/31 23:26
+Description  : 外星文字典
+"""
+
+"""
+难度：困难
+与269 alien-dictionary题目重复
+
+现有一种使用英语字母的外星文语言，这门语言的字母顺序与英语顺序不同。
+给定一个字符串列表 words ，作为这门语言的词典，words 中的字符串已经 按这门新语言的字母顺序进行了排序 。
+请你根据该词典还原出此语言中已知的字母顺序，并 按字母递增顺序 排列。若不存在合法字母顺序，返回 "" 。若存在多种可能的合法字母顺序，返回其中 任意一种 顺序即可。
+字符串 s 字典顺序小于 字符串 t 有两种情况：
+在第一个不同字母处，如果 s 中的字母在这门外星语言的字母顺序中位于 t 中字母之前，那么s 的字典顺序小于 t 。
+如果前面 min(s.length, t.length) 字母都相同，那么 s.length < t.length 时，s 的字典顺序也小于 t 。
+
+
+示例 1：
+输入：words = ["wrt","wrf","er","ett","rftt"]
+输出："wertf"
+
+示例 2：
+输入：words = ["z","x"]
+输出："zx"
+
+示例 3：
+输入：words = ["z","x","z"]
+输出：""
+解释：不存在合法字母顺序，因此返回 "" 。
+
+
+提示：
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] 仅由小写英文字母组成
+"""
+
+from typing import List
+from itertools import pairwise
+
+
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        g = {}
+        for c in words[0]:
+            g[c] = []
+        for s, t in pairwise(words):
+            for c in t:
+                g.setdefault(c, [])
+            for u, v in zip(s, t):
+                if u != v:
+                    g[u].append(v)
+                    break
+            else:
+                if len(s) > len(t):
+                    return ""
+
+        VISITING, VISITED = 1, 2
+        states = {}
+        order = []
+
+        def dfs(u: str) -> bool:
+            states[u] = VISITING
+            for v in g[u]:
+                if v not in states:
+                    if not dfs(v):
+                        return False
+                elif states[v] == VISITING:
+                    return False
+            order.append(u)
+            states[u] = VISITED
+            return True
+
+        return ''.join(reversed(order)) if all(dfs(u) for u in g if u not in states) else ""
