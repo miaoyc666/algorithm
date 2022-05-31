1696bc4663c203f12cad6aa375739905d9a90aba
miaoyc <miaoyc666@outlook.com>
2022-05-31 23:10:52 +0800

    ====refactor====

--- leetcode/python/binary-tree/_617_merge-two-binary-trees.py
+++ leetcode/python/binary-tree/_617_merge-two-binary-trees.py
@@ -10,7 +10,6 @@ Description  : 合并二叉树
 
 """
 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
-
 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为NULL 的节点将直接作为新二叉树的节点。
 
 示例1:
@@ -59,27 +58,3 @@ class Solution(object):
         root1.left = self.mergeTrees(root1.left, root2.left)
         root1.right = self.mergeTrees(root1.right, root2.right)
         return root1
-
-    #     # if not root1 and not root2:
-    #     #     return
-    #     # root = TreeNode(root1.val + root2.val)
-    #     self.root = self.dfs(self.root, root1, root2)
-    #     return self.root
-
-    # def dfs(self, root, root1, root2):
-    #     if not root1 and not root2:
-    #         return
-
-    #     if root1 and root2:
-    #         if not root:
-    #             root = TreeNode(0)
-    #         root.val = root1.val + root2.val
-    #         root.left =self.dfs(root.left, root1.left, root2.left)
-    #         root.right = self.dfs(root.right, root1.right, root2.right)
-    #         return root
-
-    #     if root1 and not root2:
-    #         return TreeNode(root1.val)
-
-    #     if not root1 and root2:
-    #         return TreeNode(root2.val)
