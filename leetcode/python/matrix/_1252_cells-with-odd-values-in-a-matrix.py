1299cb75c1989e25eb6ffa86bc6f99d42484e2cd
miaoyc <miaoyc666@outlook.com>
2022-07-14 10:49:08 +0800

    ====add matrix: cells-with-odd-values-in-a-matrix====

---
+++ leetcode/python/matrix/_1252_cells-with-odd-values-in-a-matrix.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _1252_cells-with-odd-values-in-a-matrix.py
+Author       : miaoyc
+Create date  : 2022/7/12 21:28 
+Description  : 
+"""
