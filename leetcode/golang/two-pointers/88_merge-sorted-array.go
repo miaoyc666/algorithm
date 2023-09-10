a14a2b5a8d3c710cbb02603342e0abf884a4655b
miaoyc <miaoyc666@outlook.com>
2023-09-10 21:41:35 +0800

    ====feat: add _merge-sorted-array 4 go====

---
+++ leetcode/golang/two-pointers/88_merge-sorted-array.go
@@ -0,0 +1,76 @@
+package two_pointers
+
+/*
+File name    : 88_merge-sorted-array.go
+Author       : miaoyc
+Create Date  : 2023/9/10 21:27
+Update Date  : 2023/9/10 21:27
+Description  : 合并有序数组
+*/
+
+/*
+给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+
+注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+
+示例 1：
+输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+输出：[1,2,2,3,5,6]
+解释：需要合并 [1,2,3] 和 [2,5,6] 。
+合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+
+示例 2：
+输入：nums1 = [1], m = 1, nums2 = [], n = 0
+输出：[1]
+解释：需要合并 [1] 和 [] 。
+合并结果是 [1] 。
+
+示例 3：
+输入：nums1 = [0], m = 0, nums2 = [1], n = 1
+输出：[1]
+解释：需要合并的数组是 [] 和 [1] 。
+合并结果是 [1] 。
+注意，因为 m = 0 ，所以 nums1 中没有元素。nums1 中仅存的 0 仅仅是为了确保合并结果可以顺利存放到 nums1 中。
+
+提示：
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[j] <= 109
+
+进阶：你可以设计实现一个时间复杂度为 O(m + n) 的算法解决此问题吗？
+*/
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	sorted := make([]int, 0, m+n)
+	p1, p2 := 0, 0
+	for {
+		if p1 == m {
+			// ...的功能为解序列
+			sorted = append(sorted, nums2[p2:]...)
+			break
+		}
+		if p2 == n {
+			// ...的功能为解序列
+			sorted = append(sorted, nums1[p1:]...)
+			break
+		}
+		if nums1[p1] < nums2[p2] {
+			sorted = append(sorted, nums1[p1])
+			p1++
+		} else {
+			sorted = append(sorted, nums2[p2])
+			p2++
+		}
+	}
+	copy(nums1, sorted)
+}
+
+/*
+copy小贴士:
+1.如果 dst 长度小于 src 的长度，则 拷贝src中的部分内容；
+2.如果大于，则全部拷贝过来，其余的空间填充该类型的默认值；
+3.如果相等，刚好不多不少 copy 过来，所以，通常dst在初始化时即指定其为src的长度
+*/
