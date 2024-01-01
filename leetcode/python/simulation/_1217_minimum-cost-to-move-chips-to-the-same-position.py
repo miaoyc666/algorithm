b91dce193f73744548214594a8a0995acf9d11aa
miaoyc <miaoyc666@outlook.com>
2024-01-01 12:00:28 +0800

    refactor: refomat code

--- leetcode/python/simulation/_1217_minimum-cost-to-move-chips-to-the-same-position.py
+++ leetcode/python/simulation/_1217_minimum-cost-to-move-chips-to-the-same-position.py
@@ -8,15 +8,16 @@ Create date  : 2022/7/8 21:19
 Description  : 玩筹码
 """
 
+from typing import List
+
 """
 难度：简单
 
-有 n 个筹码。第 i 个筹码的位置是 position[i] 。
-我们需要把所有筹码移到同一个位置。在一步中，我们可以将第 i 个筹码的位置从 position[i] 改变为:
-position[i] + 2 或 position[i] - 2 ，此时 cost = 0
-position[i] + 1 或 position[i] - 1 ，此时 cost = 1
+有n个筹码。第 i 个筹码的位置是position[i]。
+我们需要把所有筹码移到同一个位置。在一步中，我们可以将第 i 个筹码的位置从position[i]改变为:
+position[i] + 2或position[i] - 2，此时cost = 0
+position[i] + 1或position[i] - 1，此时cost = 1
 返回将所有筹码移动到同一位置上所需要的 最小代价 。
- 
 
 示例 1：
 输入：position = [1,2,3]
@@ -33,13 +34,12 @@ position[i] + 1 或 position[i] - 1 ，此时 cost = 1
 示例 3:
 输入：position = [1,1000000000]
 输出：1
- 
+
 
 提示：
 1 <= chips.length <= 100
 1 <= chips[i] <= 10^9
 """
-
 
 
 class Solution:
