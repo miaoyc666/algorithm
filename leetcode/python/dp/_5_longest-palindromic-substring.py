903769945701d0e97a96c1a1634e6bb3247d3767
miaoyc <miaoyc666@outlook.com>
2024-04-06 22:49:25 +0800

    init dp:longest-palindromic-substring

---
+++ leetcode/python/dp/_5_longest-palindromic-substring.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _5_longest-palindromic-substring.py
+Author       : miaoyc
+Create date  : 2024/4/6 22:42
+Create date  : 2024/4/6 22:42
+Description  : 最长回文子串
+"""
+
+"""
+给你一个字符串s，找到s中最长的回文子串。
+如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+ 
+示例 1：
+输入：s = "babad"
+输出："bab"
+解释："aba" 同样是符合题意的答案。
+
+示例 2：
+输入：s = "cbbd"
+输出："bb"
+ 
+提示：
+1 <= s.length <= 1000
+s 仅由数字和英文字母组成
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        return ""
