91f0a36673e51de200a0e75aa24181d7cb5ee33a
miaoyc <miaoyc666@outlook.com>
2024-04-10 22:46:43 +0800

    init dp: palindromic-substrings

---
+++ leetcode/python/dp/_647_palindromic-substrings.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : _647_palindromic-substrings.py
+Author       : miaoyc
+Create date  : 2024/4/10 22:44
+Create date  : 2024/4/10 22:44
+Description  : 回文子串
+"""
+
+"""
+给你一个字符串 s ，请你统计并返回这个字符串中 回文子串 的数目。
+回文字符串 是正着读和倒过来读一样的字符串。
+子字符串 是字符串中的由连续字符组成的一个序列。
+具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+
+示例 1：
+输入：s = "abc"
+输出：3
+解释：三个回文子串: "a", "b", "c"
+
+示例 2：
+输入：s = "aaa"
+输出：6
+解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
+ 
+提示：
+1 <= s.length <= 1000
+s 由小写英文字母组成
+"""
+
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        pass
